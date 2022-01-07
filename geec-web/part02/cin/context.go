1d0d3564229fb02d0538553ad46206a47cf4a6f6
chengjc <chengjichang123@qq.com>
2022-01-07 17:33:32 +0800

    将路由(router)独立出来，方便之后增强。 设计上下文(Context)，封装 Request 和 Response ，提供对 JSON、HTML 等返回类型的支持。

---
+++ geec-web/part02/cin/context.go
@@ -0,0 +1,34 @@
+package cin
+
+import "net/http"
+
+//对Web服务来说，无非是根据请求*http.Request，构造响应http.ResponseWriter。
+//但是这两个对象提供的接口粒度太细，比如我们要构造一个完整的响应，
+//需要考虑消息头(Header)和消息体(Body)
+//Header 包含了状态码(StatusCode)，消息类型(ContentType)等几乎每次请求都需要设置的信息。
+//因此，如果不进行有效的封装，那么框架的用户将需要写大量重复，繁杂的代码，而且容易出错。
+//针对常用场景，能够高效地构造出 HTTP 响应是一个好的框架必须考虑的点。
+
+//封装*http.Request和http.ResponseWriter的方法，简化相关接口的调用，只是设计 Context 的原因之一。
+//对于框架来说，还需要支撑额外的功能。例如，将来解析动态路由/hello/:name，参数:name的值放在哪呢？
+//再比如，框架需要支持中间件，那中间件产生的信息放在哪呢？
+//Context 随着每一个请求的出现而产生，请求的结束而销毁，和当前请求强相关的信息都应由 Context 承载。
+//因此，设计 Context 结构，扩展性和复杂性留在了内部，而对外简化了接口。
+//路由的处理函数，以及将要实现的中间件，参数都统一使用 Context 实例
+//Context 就像一次会话的百宝箱，可以找到任何东西。
+type H map[string]interface{}
+
+type Context struct {
+	Writer     http.ResponseWriter
+	Req        *http.Request
+	Path       string
+	Method     string
+	StatusCode int
+}
+
+func newContext(w http.ResponseWriter, req *http.Request) *Context {
+	return &Context{Writer: w,
+		Req:    req,
+		Path:   req.URL.Path,
+		Method: req.Method}
+}
